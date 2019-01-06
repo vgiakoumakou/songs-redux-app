81b9aa893e593250a6baf9dabec13dbf34799806
vgiakoumakou <v.giakoumakou@ihu.edu.gr>
2019-01-06 23:00:36 +0200

    First commit with initial components, reducers and actions

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import { selectSong } from '../actions';
+import SongList from './SongList';
+
+const App = () => {
+    return (
+        <div>
+            <SongList />
+        </div>
+    );
+}
+
+export default App;
